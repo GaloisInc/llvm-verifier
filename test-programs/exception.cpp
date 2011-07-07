e223ab83a2260ab96f7d1959bc0ae7b8f5407b18
Joe Hendrix <jhendrix@galois.com>
2011-07-07 11:44:55 -0700

    Add a few test programs to understand clang code generation.

---
+++ test-programs/exception.cpp
@@ -0,0 +1,35 @@
+#include <exception>
+#include <string>
+
+/** Class for exceptions. */
+class my_exception_t : public std::exception {
+private:
+  std::string msg_;
+public:
+  my_exception_t(const char* msg) : msg_(msg) {}
+
+  virtual ~my_exception_t() throw() {}
+
+
+  virtual const char* what() const throw() { return msg_.c_str(); }
+};
+
+void throw_except() {
+  throw my_exception_t("Throw Exception");
+}
+
+void catch_except() {
+  try {
+    throw_except();
+  } catch (std::exception& e) {
+  }
+}
+
+int try_and_throw_except() {
+  try {
+    throw my_exception_t("Throw Exception");
+    return 0;
+  } catch (std::exception& e) {
+    return -1;
+  }
+}
