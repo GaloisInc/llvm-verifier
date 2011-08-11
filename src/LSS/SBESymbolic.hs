cacdb1ef11460fb9bc9e98b8bde94e41979c92b7
Aaron Tomb <atomb@galois.com>
2011-08-11 11:12:19 -0700

    Refactor symbolic backend slightly.
    The external interface is the same.

--- src/LSS/SBESymbolic.hs
+++ src/LSS/SBESymbolic.hs
@@ -9,14 +9,13 @@ Point-of-contact : atomb
 
 module LSS.SBESymbolic where
 
-import Control.Monad
+import Control.Applicative
 import qualified Text.LLVM.AST as LLVM
 import qualified Verinf.Symbolic as S
-import qualified Verinf.Symbolic.Common as C
 import LSS.SBEInterface
 
 --------------------------------------------------------------------------------
--- Symbolic backend
+-- Word-level symbolic backend
 
 type instance SBETerm S.SymbolicMonad = S.SymbolicTerm
 type instance SBEMemory S.SymbolicMonad = S.SymbolicTerm
@@ -59,26 +58,24 @@ sbeSymbolic = SBE
 liftSBESymbolic :: S.SymbolicMonad a -> IO a
 liftSBESymbolic = S.runSymbolic
 
+--------------------------------------------------------------------------------
+-- Bit-level symbolic backend
+
+-- BitMonad is just a wrapper to allow SBETerm and SBEMemory
+-- associations.
 newtype BitMonad a = BM { runBitMonad :: IO a }
 type instance SBETerm BitMonad = S.LitResult S.Lit
 type instance SBEMemory BitMonad = S.LitResult S.Lit
 
 -- | Symbolic interface with all operations at the bit level.
--- TODO: This feels like it's breaking abstractions.
 sbeSymbolicBit :: S.BitEngine S.Lit -> SBE BitMonad
 sbeSymbolicBit be = SBE
   { termInt  = \w -> BM . return . S.concreteValueLit be . S.CInt (S.Wx w)
   , termBool = BM . return . S.concreteValueLit be . S.mkCBool
   , applyIte = undefined
   , applyICmp = undefined
-  , applyBitwise = undefined
-  , applyArith =
-    \op a b -> case op of
-                 LLVM.Add -> BM $ C.fromLsbfV `liftM`
-                             S.beAddInt be (C.toLsbfV a) (C.toLsbfV b)
-                 _ -> error $
-                      "unsupported arithmetic op: " ++
-                      show op
+  , applyBitwise = bitBitwise be
+  , applyArith = bitArith be
   , memInitMemory = BM $ return undefined
   , memAlloca = \_mem _eltType _len _a -> BM $ return undefined
   , memLoad = \_mem _ptr -> BM $ return undefined
@@ -88,6 +85,32 @@ sbeSymbolicBit be = SBE
   , memBlockAddress = \_mem _s _b -> BM $ return undefined
   , memSelect = \_t _mem _mem' -> BM $ return undefined
   }
+
+bitArith :: S.BitEngine S.Lit -> LLVM.ArithOp
+         -> S.LitResult S.Lit -> S.LitResult S.Lit
+         -> BitMonad (S.LitResult S.Lit)
+bitArith be op (S.LV a) (S.LV b) = BM $ S.LV <$> f be a b
+  where f = case op of
+              LLVM.Add -> S.beAddInt
+              LLVM.Mul -> S.beMulInt
+              _ -> error $ "unsupported arithmetic op: " ++ show op
+bitArith _ op (S.LVN _) _ =
+  error $ "arithmetic op " ++ show op ++ " applied to non-scalar"
+bitArith _ op _ (S.LVN _) =
+  error $ "arithmetic op " ++ show op ++ " applied to non-scalar"
+
+bitBitwise :: S.BitEngine S.Lit -> LLVM.BitOp
+           -> S.LitResult S.Lit -> S.LitResult S.Lit
+           -> BitMonad (S.LitResult S.Lit)
+bitBitwise be op (S.LV a) (S.LV b) = BM $ S.LV <$> f be a b
+  where f = case op of
+              LLVM.And -> S.beAndInt
+              LLVM.Or -> S.beOrInt
+              _ -> error $ "unsupported arithmetic op: " ++ show op
+bitBitwise _ op (S.LVN _) _ =
+  error $ "bitwise op " ++ show op ++ " applied to non-scalar"
+bitBitwise _ op _ (S.LVN _) =
+  error $ "bitwise op " ++ show op ++ " applied to non-scalar"
 
 liftSBESymbolicBit :: BitMonad a -> IO a
 liftSBESymbolicBit = runBitMonad
