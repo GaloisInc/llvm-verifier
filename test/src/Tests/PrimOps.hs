6a929927d731df79b21be85646557dba2215cae1
Joel Stanley <jstanley@galois.com>
2011-08-13 09:58:31 -0500

    minor test harness reorg

--- test/src/Tests/PrimOps.hs
+++ test/src/Tests/PrimOps.hs
@@ -14,7 +14,6 @@ import           Control.Monad.Trans
 import           Data.Int
 import           LSS.Execution.Codebase
 import           LSS.Execution.Common
-import           LSS.Execution.Utils
 import           LSS.SBESymbolic
 import           LSS.Simulator
 import           System.FilePath
@@ -22,71 +21,51 @@ import           Test.QuickCheck
 import           Test.QuickCheck.Monadic
 import           Tests.Common
 import           Text.LLVM               ((=:))
-import           Verinf.Symbolic.Common  ( PrettyTerm(..)
-                                         , getSVal
-                                         )
+import           Verinf.Symbolic         (SymbolicTerm)
+import           Verinf.Symbolic.Common  (getSVal)
 import qualified Text.LLVM               as L
 
 i32 :: L.Type
 i32 = L.iT 32
 
-int32 :: Gen Int32
-int32 = arbitrary
-
 primOpTests :: [(Args, Property)]
 primOpTests =
   [ test 10 False "concrete int32 add" $
-      binCInt32Fn "primOps.bc"  (L.Symbol "int32_add") (+)
+      chkBinCInt32Fn "primOps.bc"  (L.Symbol "int32_add") (+)
   , test 10 False "concrete int32 sqr" $
-      unaryCInt32Fn "primOps.bc" (L.Symbol "int32_square") sqr
+      chkUnaryCInt32Fn "primOps.bc" (L.Symbol "int32_square") sqr
   , test 10 False "concrete int32 muladd" $
-      binCInt32Fn "primOps.bc" (L.Symbol "int32_muladd") (\x y -> sqr (x + y))
+      chkBinCInt32Fn "primOps.bc" (L.Symbol "int32_muladd") (\x y -> sqr (x + y))
   ]
   where
     sqr x = x * x
 
-binCInt32Fn :: FilePath -> L.Symbol -> (Int32 -> Int32 -> Int32) -> PropertyM IO ()
-binCInt32Fn bcFile sym chkOp = do
-  forAllM int32 $ \x -> do
-  forAllM int32 $ \y -> do
-  mrv <- run $ do
-    cb <- loadCodebase $ supportDir </> bcFile
-    runSimulator cb sbeSymbolic (SM . lift . liftSBESymbolic) $ do
-      i1 <- withSBE $ \sbe -> termInt sbe 32 (fromIntegral x)
-      i2 <- withSBE $ \sbe -> termInt sbe 32 (fromIntegral y)
-      callDefine sym i32 [ i32 =: i1 , i32 =: i2 ]
-      getProgramReturnValue
-  case mrv of
-    Just (getSVal -> Just v) ->
-      assert $ v == fromIntegral (x `chkOp` y)
-    _ ->
-      assertMsg False $ show (L.ppSymbol sym) ++ ": unexpected return value"
+chkBinCInt32Fn :: FilePath -> L.Symbol -> (Int32 -> Int32 -> Int32) -> PropertyM IO ()
+chkBinCInt32Fn bcFile sym chkOp = forAllM arbitrary $ \(x,y) -> do
+  chkRslt sym (fromIntegral (x `chkOp` y))
+    =<< run (runCInt32Fn bcFile sym [x, y])
 
---runCInt32Fn :: FilePath -> L.Symbol -> [Int32] ->
+chkUnaryCInt32Fn :: FilePath -> L.Symbol -> (Int32 -> Int32) -> PropertyM IO ()
+chkUnaryCInt32Fn bcFile sym chkOp = forAllM arbitrary $ \x -> do
+  chkRslt sym (fromIntegral (chkOp x))
+    =<< run (runCInt32Fn bcFile sym [x])
+
+chkNullaryCInt32Fn :: FilePath -> L.Symbol -> Int32 -> PropertyM IO ()
+chkNullaryCInt32Fn bcFile sym chkVal = do
+  chkRslt sym (fromIntegral chkVal) =<< run (runCInt32Fn bcFile sym [])
+
+runCInt32Fn :: FilePath -> L.Symbol -> [Int32] -> IO (Maybe SymbolicTerm)
 runCInt32Fn bcFile sym cargs = do
-    cb <- loadCodebase $ supportDir </> bcFile
-    runSimulator cb sbeSymbolic (SM . lift . liftSBESymbolic) $ do
-      args <- withSBE $ \sbe -> mapM (termInt sbe 32 . fromIntegral) cargs
-      callDefine sym i32 $ map (\x -> i32 =: x) args
-      getProgramReturnValue
+  cb <- loadCodebase $ supportDir </> bcFile
+  runSimulator cb sbeSymbolic (SM . lift . liftSBESymbolic) $ do
+    args <- withSBE $ \sbe -> mapM (termInt sbe 32 . fromIntegral) cargs
+    callDefine sym i32 $ map (\x -> i32 =: x) args
+    getProgramReturnValue
 
-unaryCInt32Fn :: FilePath -> L.Symbol -> (Int32 -> Int32) -> PropertyM IO ()
-unaryCInt32Fn bcFile sym chkOp = do
-  forAllM int32 $ \x -> do
-  mrv <- run $ do
-    cb <- loadCodebase $ supportDir </> bcFile
-    runSimulator cb sbeSymbolic (SM . lift . liftSBESymbolic) $ do
-     i1 <- withSBE $ \sbe -> termInt sbe 32 (fromIntegral x)
-     callDefine sym i32 [ i32 =: i1 ]
-     getProgramReturnValue
-  case mrv of
-    Just (getSVal -> Just v) ->
-      assert $ v == fromIntegral (chkOp x)
-    _ ->
-      assertMsg False $ show (L.ppSymbol sym) ++ ": unexpected return value"
-
-nullaryCInt32Fn :: FilePath -> L.Symbol -> Int32 -> PropertyM IO ()
-nullaryCInt32Fn bcFile sym chkVal = undefined
+chkRslt :: L.Symbol -> Integer -> Maybe SymbolicTerm -> PropertyM IO ()
+chkRslt _ chk (Just (getSVal -> Just v)) = assert $ v == chk
+chkRslt sym _ _                          = assertMsg False
+                                           $ show (L.ppSymbol sym) ++ ": unexpected return value"
 
 --------------------------------------------------------------------------------
 -- Scratch
