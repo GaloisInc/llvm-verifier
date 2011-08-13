27794b55ee1e6e965b46bf468584f274a4c4bf0b
Joel Stanley <jstanley@galois.com>
2011-08-12 19:01:54 -0500

    - Add initial test harness - Track llvm-pretty changes (call instructions now typed with full ptr-to-fn type) - Remove placeholder Driver.hs - Add Makefile in root of LLVM hierarchy for building/testing - Add debug verbosity levels via a LogMonad instance for the Simulator monad

---
+++ test/src/Tests.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE Rank2Types #-}
+
+module Main where
+
+import Control.Monad
+import System.Environment   (getArgs)
+import System.Exit
+import Test.QuickCheck
+import Test.QuickCheck.Test
+
+import Tests.PrimOps
+
+main :: IO ()
+main = do
+  tags <- getArgs
+  runTests (\(t, _) -> if null tags then True else t `elem` tags)
+
+runTests :: (forall a. ((String, a) -> Bool)) -> IO ()
+runTests chooseTest = do
+  results <- fmap concat $ forM qcProps $ \(desc, props) -> do
+             putStrLn $ "=== " ++ desc ++ " ==="
+             mapM (uncurry quickCheckWithResult) props
+  when (noOfTests /= noOfSelected) $
+    putStrLn $ "Selected " ++ show noOfSelected ++ " of "
+               ++ show noOfTests ++ " test categories."
+  if all isSuccess results
+    then putStrLn "All tests successful."     >> exitWith ExitSuccess
+    else putStrLn "One or more tests failed." >> exitWith (ExitFailure 1)
+  where
+    noOfTests    = length allTests
+    noOfSelected = length qcProps
+    qcProps      = filter chooseTest allTests
+    allTests     = [ ("PrimOps", primOpTests)
+                   ]
+
+
+
+
