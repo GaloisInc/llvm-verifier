909edf8044011365c903f15fc9e9037de7497d9b
Joel Stanley <jstanley@galois.com>
2011-08-27 17:10:33 -0500

    Add symbolic read test (currently disabled)

--- test/src/Tests/Symbolic.hs
+++ test/src/Tests/Symbolic.hs
@@ -24,9 +24,12 @@ symTests :: [(Args, Property)]
 symTests =
   [
     test 1 False "test-trivial-divergent-branch" $ trivBranch 1
+    -- symbolic reads not yet supported, so this is currently disabled
+  , test 1 False "test-trivial-symbolic-read"    $ trivSymRd 0
   ]
   where
     trivBranch v = psk v $ runSimple v trivBranchImpl
+    trivSymRd  v = psk v $ runSimple v trivSymRdImpl
     runSimple v  = runBitBlastSimTest v "test-sym-simple.bc"
 
 trivBranchImpl :: StdBitBlastTest
@@ -43,6 +46,21 @@ trivBranchImpl _be = do
             q     = fmap (getSVal . closeTerm sbe) . flip (evalAiger sbe) rv
         liftM2 (,) (q inps0) (q inps1)
       return (r0 == Just 0 && r1 == Just 1)
+
+trivSymRdImpl :: StdBitBlastTest
+trivSymRdImpl _be = do
+  b <- withSBE $ \sbe -> freshInt sbe 32
+  callDefine (L.Symbol "sym_read") i32 [i32 =: b]
+  mrv <- getProgramReturnValue
+  case mrv of
+    Nothing -> dbugM "No return value (fail)" >> return False
+    Just rv -> do
+      (r0, r1) <- withSBE $ \sbe -> do
+        let inps0 = replicate 32 False
+            inps1 = replicate 31 False ++ [True]
+            q     = fmap (getSVal . closeTerm sbe) . flip (evalAiger sbe) rv
+        liftM2 (,) (q inps0) (q inps1)
+      return (r0 == Just 99 && r1 == Just 42)
 
 --------------------------------------------------------------------------------
 -- Scratch
