e95b44dc2c3cf18d9f17ef30e9cb3bb1c387bb43
Joel Stanley <jstanley@galois.com>
2011-08-29 17:31:12 -0700

    Array of structs test, support for ValString

--- test/src/Tests/Aggregates.hs
+++ test/src/Tests/Aggregates.hs
@@ -26,6 +26,7 @@ aggTests =
   , test 1 False "test-array-2d-initializer"   $ arrayInit2D      1
   , test 1 False "test-array-mat4x4-mult"      $ arrayMat4x4      1
   , test 1 False "test-struct-init-and-access" $ structInitAccess 1
+  , test 1 False "test-array-of-structs"       $ structArray      1
   ]
   where
     arrayBaseIdx v        = t1 v "arr1" (Just 42)
@@ -34,6 +35,7 @@ aggTests =
     arrayInit2D v         = t1 v "twodim_init" (Just 21)
     arrayMat4x4 v         = t2 v "matrix_mul_4x4" (Just 304)
     structInitAccess v    = psk v $ runStruct v structInitAccessImpl
+    structArray v         = psk v $ runStruct v structArrayImpl
     t1                    = mkNullaryTest "test-arrays.bc"
     t2                    = mkNullaryTest "test-mat4x4.bc"
     runStruct v           = runBitBlastSimTest v "test-structs.bc"
@@ -46,18 +48,19 @@ structInitAccessImpl be = do
   case mrv of
     Nothing -> dbugM "No return value (fail)" >> return False
     Just rv -> do
---       dbugTerm "rv" rv
-
       [L.Typed _ bx, L.Typed _ by, _] <- do
         withSBE $ \sbe -> termDecomp sbe [i32, i8, padTy 3] rv
-
---       dbugTerm "bx" bx
---       dbugTerm "by" by
       return $ bx `teq` 42 && by `teq` fromIntegral (fromEnum 'z')
   where
-    close t = BitTermClosed (be, t)
-    teq t v = close t `constTermEq` v
+    teq t v = BitTermClosed (be, t) `constTermEq` v
 
+structArrayImpl :: StdBitBlastTest
+structArrayImpl be = do
+  callDefine (L.Symbol "struct_test_two") i32 []
+  mrv <- getProgramReturnValue
+  case mrv of
+    Nothing -> dbugM "No return value (fail)" >> return False
+    Just rv -> return $ BitTermClosed (be, rv) `constTermEq` 1
 
 --------------------------------------------------------------------------------
 -- Scratch
