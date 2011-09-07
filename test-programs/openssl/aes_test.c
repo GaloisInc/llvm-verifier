11d88dcdb78673d7d011f80c6c6e2a056532bd76
Aaron Tomb <atomb@galois.com>
2011-09-07 10:49:53 -0700

    Successfully equivalence-check AES.
    Cryptol reference versus both SBV-generated C code from Cryptol and OpenSSL.

--- test-programs/openssl/aes_test.c
+++ test-programs/openssl/aes_test.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <openssl/aes.h>
+#include "sym-api.h"
 
 void printBlock(const unsigned char out[16]) {
   unsigned i;
@@ -18,19 +19,23 @@ void dummy_memset(unsigned char *buf, unsigned char v, unsigned char sz) {
 
 int main(int argc, char** argv) {
 
-  unsigned char userKey[16];
-  unsigned char in[16];
+  //unsigned char userKey[16];
+  //unsigned char in[16];
   AES_KEY key;
   unsigned char out[16];
 
-  dummy_memset(userKey, 0, 16);
-  userKey[15] = 1;
-  dummy_memset(in, 0, 16);
-  in[15] = 2;
+  //dummy_memset(userKey, 0, 16);
+  //userKey[15] = 1;
+  //dummy_memset(in, 0, 16);
+  //in[15] = 2;
+
+  unsigned char *in = fresh_array_uint8(16, 0);
+  unsigned char *userKey = fresh_array_uint8(16, 0);
 
   AES_set_encrypt_key(userKey, 128, &key);
   AES_encrypt(in, out, &key);
   printBlock(out);
+  write_aiger_array_uint8(out, 16, "aes.aig");
   printf("\n");
   return 0;
 }
