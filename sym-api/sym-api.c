c90888ea906286b90d8660f232aa6cc68e006d2b
Aaron Tomb <atomb@galois.com>
2011-09-13 09:15:29 -0700

    Keep symbolic API impl consistent with header.

--- sym-api/sym-api.c
+++ sym-api/sym-api.c
@@ -71,5 +71,5 @@ void lss_override_function_by_name (char *from, char *to) {}
 void lss_override_function_by_addr (void *from, void *to) {}
 
 void lss_print_symbolic (void *sym) {}
-void lss_abort () {}
+void lss_abort (const char *msg) {}
 void lss_show_path() {}
