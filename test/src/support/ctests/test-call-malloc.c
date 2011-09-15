94da964471de56a9317bc1f8135a57d8f3970622
Joel Stanley <jstanley@galois.com>
2011-09-15 10:30:21 -0500

    add lss_show_mem(), lss_set_verbosity() sigfuns; wibbles

--- test/src/support/ctests/test-call-malloc.c
+++ test/src/support/ctests/test-call-malloc.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <sym-api.h>
 
 void f(int *a) {
     int i;
@@ -7,7 +8,9 @@ void f(int *a) {
 }
 
 int main() {
+    lss_show_mem();
     int *a = malloc(2 * sizeof(int));
+    lss_show_mem();
     a[0] = 30991;
     a[1] = 3298;
     f(a);
