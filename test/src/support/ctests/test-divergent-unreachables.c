82d3cb13e65463ea7ec5ad2bffae8d22e689aadd
Joel Stanley <jstanley@galois.com>
2011-09-23 16:57:23 -0500

    - Dummy exit node handling: also pass-through dummy exits to exit frames - Minor fixes to path numbering for debug output clarity - Added override for __assert_rtn() - Added test-divergent-unreachables unit test

---
+++ test/src/support/ctests/test-divergent-unreachables.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+#include <sym-api.h>
+
+int main(int argc, char** argv) 
+{
+    uint8_t x = lss_fresh_uint8(0);
+
+    if (x > 5) {
+    }
+    else {
+        /* if x > 5, then the assertion passes but the rest of the path */
+        /* is infeasible, and should be reported as such by LSS. */
+        /* if x < 5, then the assertion failure should trigger. */
+        /* Thus, LSS should report two path errors for this program. */
+        assert (x > 5); 
+        /* unreachable */
+    }
+    return 1;
+}
