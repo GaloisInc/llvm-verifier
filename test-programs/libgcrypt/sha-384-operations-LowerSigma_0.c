cb58619cd08c3e64a826d0a5cf9dc83abd887f93
Joel Stanley <jstanley@galois.com>
2011-10-18 21:37:54 -0700

    Initial forays into SHA384 proofs:   - Proof completes for 1 byte messages using non-unrolled libgcrypt transform()
    Also, moved libgcrypt aes goop into libgcrypt/aes/ subdirectory.  That
    AES code won't build/run anymore, but we will revisit it when we need
    to.

---
+++ test-programs/libgcrypt/sha-384-operations-LowerSigma_0.c
@@ -0,0 +1,10 @@
+#include "sha512.c"
+#include "sym-api.h"
+
+int main(int argc, char** argv) 
+{
+    uint64_t x = lss_fresh_uint64(0);
+
+    uint64_t r = S0(x);
+    lss_write_aiger_uint64(r, "gcrypt_impl_AIGs/LowerSigma_0.aig");
+}
---
+++ test-programs/libgcrypt/sha-384-operations-sha384_top.c
@@ -0,0 +1,22 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sym-api.h>
+
+#include "sha512.c"
+
+int main() {
+
+	byte cxt[512] = {0};
+	byte *res;
+	byte *text = lss_fresh_array_uint8(1, 0, NULL);
+
+	sha384.init(cxt);
+	sha384.write(cxt, text, 1);
+	sha384.final(cxt);
+
+	res = sha384.read(cxt);
+
+	lss_write_aiger_array_uint8(res, 48, "gcrypt_impl_AIGs/sha384_top.aig");
+	return 0;
+}
