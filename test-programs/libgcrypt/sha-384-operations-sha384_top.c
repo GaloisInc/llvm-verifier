d15d8d48caccc503674b60ef27b94771f7528c9a
Joel Stanley <jstanley@galois.com>
2011-10-18 22:35:02 -0700

    libgcrypt sha384 Makefile cleanups

--- test-programs/libgcrypt/sha-384-operations-sha384_top.c
+++ test-programs/libgcrypt/sha-384-operations-sha384_top.c
@@ -5,18 +5,18 @@
 
 #include "sha512.c"
 
-int main() {
+int main()
+{
+    byte cxt[512] = {0};
+    byte *res;
+    byte *text = lss_fresh_array_uint8(1, 0, NULL);
 
-	byte cxt[512] = {0};
-	byte *res;
-	byte *text = lss_fresh_array_uint8(1, 0, NULL);
+    sha384.init(cxt);
+    sha384.write(cxt, text, 1);
+    sha384.final(cxt);
 
-	sha384.init(cxt);
-	sha384.write(cxt, text, 1);
-	sha384.final(cxt);
+    res = sha384.read(cxt);
 
-	res = sha384.read(cxt);
-
-	lss_write_aiger_array_uint8(res, 48, "gcrypt_impl_AIGs/sha384_top.aig");
-	return 0;
+    lss_write_aiger_array_uint8(res, 48, "gcrypt_impl_AIGs/sha384_top.aig");
+    return 0;
 }
