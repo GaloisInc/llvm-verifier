21412b2ced2bf16c1b8f6f806ded9a1e64886c2f
jhendrix <jhendrix@galois.com>
2012-05-25 17:58:39 -0700

    Update LLVM cabal file to depend on library for faster build times.

---
+++ lss/LSSImpl.hs
@@ -0,0 +1,148 @@
+{- |
+Module           : $Header$
+Description      : Implementation details for the command line driver;
+                   primarily to facilitate programmatic invocation
+Stability        : provisional
+Point-of-contact : jstanley
+-}
+
+{-# LANGUAGE DeriveDataTypeable         #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE Rank2Types                 #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE ViewPatterns               #-}
+{-# LANGUAGE ExistentialQuantification  #-}
+
+module LSSImpl where
+
+import           Control.Applicative             hiding (many)
+import           Control.Monad.State
+import           Data.Int
+import           Data.LLVM.Memory
+import           Data.LLVM.Symbolic.AST
+import           LSS.Execution.Codebase
+import           LSS.Execution.Common
+import           LSS.LLVMUtils
+import           LSS.SBEBitBlast
+import           LSS.Simulator
+import           Numeric
+import           System.Console.CmdArgs.Implicit hiding (args, setVerbosity, verbosity)
+import           Text.LLVM                       ((=:), Typed(..))
+import           Verinf.Symbolic.Common          (ConstantProjection(..))
+import           Verinf.Utils.LogMonad
+import qualified Data.Vector.Storable as SV
+import qualified Text.LLVM                       as L
+
+data LSS = LSS
+  { dbug    :: DbugLvl
+--   , stack   :: StackSz
+  , argv     :: String
+  , mname    :: Maybe String
+  , memtype  :: Maybe String
+  , errpaths :: Bool
+  , xlate    :: Bool
+  } deriving (Show, Data, Typeable)
+
+newtype DbugLvl = DbugLvl { unD :: Int32 }
+  deriving (Data, Enum, Eq, Integral, Num, Ord, Real, Show, Typeable)
+instance Default DbugLvl where def = DbugLvl 1
+
+-- newtype StackSz = StackSz { unS :: Int32 }
+--   deriving (Data, Enum, Eq, Integral, Num, Ord, Real, Show, Typeable)
+-- instance Default StackSz where def = StackSz 8
+
+data ExecRslt sbe crt
+  = NoMainRV [ErrorPath sbe] (Maybe (SBEMemory sbe))
+  | SymRV    [ErrorPath sbe] (Maybe (SBEMemory sbe)) (SBETerm sbe)
+  | ConcRV   [ErrorPath sbe] (Maybe (SBEMemory sbe)) crt
+
+type ExecRsltHndlr sbe crt a =
+     SBE sbe          -- ^ SBE that was used used during a test
+  -> SBEMemory sbe    -- ^ Typed initial memory that was used during a test
+  -> ExecRslt sbe crt -- ^ Execution results; final memory is embedded here
+  -> IO a
+
+lssImpl :: (Eq l, SV.Storable l)
+        => SBE (BitIO mem l)
+        -> SBEMemory (BitIO mem l)
+        -> Codebase
+        -> [String]
+        -> MemType
+        -> LSS
+        -> IO (ExecRslt (BitIO mem l) Integer)
+lssImpl sbe mem cb argv0 _memType args = do
+  mainDef <- case lookupDefine' (L.Symbol "main") cb of
+               Nothing -> error "Provided bitcode does not contain main()."
+               Just mainDef -> do
+                 when (null (sdArgs mainDef) && length argv' > 1) warnNoArgv
+                 return mainDef
+  runBitBlast sbe mem cb mg argv' args mainDef
+  where
+    argv' = "lss" : argv0
+    lc    = cbLLVMCtx cb
+    mg    = defaultMemGeom lc
+
+runBitBlast :: (Eq l, SV.Storable l)
+            => BitBlastSBE mem l -- ^ SBE to use
+            -> mem               -- ^ SBEMemory to use
+            -> Codebase
+            -> MemGeom
+            -> [String]          -- ^ argv
+            -> LSS               -- ^ LSS command-line arguments
+            -> SymDefine         -- ^ Define of main()
+            -> IO (ExecRslt (BitIO mem l) Integer)
+runBitBlast sbe mem cb mg argv' args mainDef = do
+  runSimulator cb sbe mem liftBitBlastSim seh' opts $ do
+    setVerbosity $ fromIntegral $ dbug args
+
+    whenVerbosity (>=5) $ do
+      let sr (a,b) = "[0x" ++ showHex a "" ++ ", 0x" ++ showHex b "" ++ ")"
+      dbugM $ "Memory model regions:"
+      dbugM $ "Stack range : " ++ sr (mgStack mg)
+      dbugM $ "Code range  : " ++ sr (mgCode mg)
+      dbugM $ "Data range  : " ++ sr (mgData mg)
+      dbugM $ "Heap range  : " ++ sr (mgHeap mg)
+
+    callDefine_ (L.Symbol "main") i32 $
+      if mainHasArgv then buildArgv numArgs argv' else return []
+
+    mrv <- getProgramReturnValue
+    mm  <- getProgramFinalMem
+    eps <- gets errorPaths
+    case mrv of
+      Nothing -> return (NoMainRV eps mm)
+      Just rv -> do
+        let mval = getUVal . closeTerm sbe $ rv
+        return $ maybe (SymRV eps mm rv) (\x -> (ConcRV eps mm x)) mval
+  where
+    opts        = Just $ LSSOpts (errpaths args)
+    seh'        = defaultSEH
+    mainHasArgv = not $ null $ sdArgs mainDef
+    numArgs     = fromIntegral (length argv') :: Int32
+
+buildArgv ::
+  ( MonadIO m
+  , Functor sbe
+  , Functor m
+  , ConstantProjection (SBEClosedTerm sbe)
+  )
+  => Int32 -> [String] -> Simulator sbe m [Typed (SBETerm sbe)]
+buildArgv numArgs argv' = do
+  argc     <- withSBE $ \s -> termInt s 32 (fromIntegral numArgs)
+  strVals  <- mapM (getTypedTerm' Nothing . cstring) argv'
+  strPtrs  <- mapM (\ty -> tv <$> alloca ty Nothing Nothing) (tt <$> strVals)
+  num      <- getTypedTerm (int32const numArgs)
+  argvBase <- alloca i8p (Just num) Nothing
+  argvArr  <- (L.Array numArgs i8p =:) <$> withSBE (\s -> termArray s strPtrs)
+  -- Write argument string data and argument string pointers
+  forM_ (strPtrs `zip` strVals) $ \(p,v) -> store v p
+  store argvArr (tv argvBase)
+  return [i32 =: argc, argvBase]
+  where
+    tv = typedValue
+    tt = typedType
+
+warnNoArgv :: IO ()
+warnNoArgv = putStrLn "WARNING: main() takes no argv; ignoring provided arguments."
---
+++ lss/Main_lss.hs
@@ -0,0 +1,116 @@
+{- |
+Module           : $Header$
+Description      : The command line driver for the LLVM symbolic simulator
+Stability        : provisional
+Point-of-contact : jstanley
+-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE DeriveDataTypeable         #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ViewPatterns               #-}
+
+module Main where
+
+import           Control.Applicative             hiding (many)
+import           Control.Monad
+import           Data.Char
+import           Data.LLVM.Symbolic.AST
+import           Data.LLVM.Symbolic.Translation  (liftDefine)
+import           LSS.Execution.Codebase
+import           LSS.Execution.Utils
+import           LSS.SBEInterface
+import           LSS.SBEBitBlast
+import           LSSImpl
+import           System.Console.CmdArgs.Implicit hiding (args, setVerbosity, verbosity)
+import           System.Environment              (getArgs)
+import           System.Exit
+import           Text.ParserCombinators.Parsec
+import           Verinf.Symbolic                 (createBitEngine)
+import qualified System.Console.CmdArgs.Implicit as Args
+import qualified Text.LLVM                       as L
+
+main :: IO ()
+main = do
+  numArgs <- length <$> getArgs
+  when (numArgs == 0) $ do
+    putStrLn $ "lss: No command line options given. Try --help for more information."
+    exitFailure
+
+  args <- cmdArgs $ LSS
+            { dbug    = def &= help "Debug verbosity level (1-7)"
+--             , stack   = def &= opt "8" &= help "Stack size in megabytes (default: 8)"
+            , argv     = def &= typ "\"arg1 arg2 ...\""
+                             &= help "Space-delimited arguments to main()"
+            , memtype  = def &= typ "[bitblast|dagbased]"
+                             &= help "Memory model to use (default: bitblast)"
+            , errpaths = def &= help "Dump error path details upon program completion (potentially very verbose)."
+            , xlate    = def &= help "Prints the symbolic AST translation to stdout, and then terminates."
+            , mname    = def &= typ "Fully-linked .bc containing main()"
+                             &= Args.args
+            }
+            &= summary ("LLVM Symbolic Simulator (lss) 0.1b Oct 2011. "
+                        ++ "Copyright 2011 Galois, Inc. All rights reserved.")
+
+  let eatWS (' ':cs) = eatWS cs
+      eatWS cs       = cs
+
+  memType <- case eatWS <$> memtype args of
+    Just "dagbased" -> return BitBlastDagBased
+    Just "bitblast" -> return BitBlastBuddyAlloc
+    Nothing         -> return BitBlastBuddyAlloc
+    _               -> do
+      putStrLn "Invalid memory model specified.  Please choose 'bitblast' or 'dagbased'."
+      exitFailure
+
+  bcFile <- case mname args of
+    Nothing -> do putStrLn $ "lss: No LLVM bitcode file provided. "
+                             ++ "Try --help for more information."
+                  exitFailure
+    Just nm -> return nm
+
+  let p     = many $ between spaces spaces $ many1 $ satisfy $ not . isSpace
+      argv' = case runParser p () "argv values" (argv args) of
+                Left _e -> error "Unable to parse command-line arguments (argv)."
+                Right x -> x
+
+  cb <- loadCodebase bcFile
+
+  when (xlate args) $ do
+    -- Dump the translated module and exit
+    let via s f = mapM_ (putStrLn . show  . f) (s $ origModule cb)
+    ((:[]) . L.modDataLayout) `via` L.ppDataLayout
+    L.modTypes                `via` L.ppTypeDecl
+    L.modGlobals              `via` L.ppGlobal
+    L.modDeclares             `via` L.ppDeclare
+    L.modDefines              `via` (ppSymDefine . liftDefine)
+    exitWith ExitSuccess
+
+  be <- createBitEngine
+  let lc = cbLLVMCtx cb
+      mg = defaultMemGeom lc
+      processRslt sbe execRslt = do
+        case execRslt of
+            NoMainRV _eps _mm -> do
+              unless (dbug args == 0) $
+                dbugM "Obtained no return value from main()."
+              _ <- exitWith ExitSuccess
+              return ()
+            SymRV _eps _mm rv -> do
+              unless (dbug args == 0) $
+                dbugM "Obtained symbolic return value from main():"
+              dbugM $ show $ prettyTermD sbe rv
+              _ <- exitWith ExitSuccess
+              return ()
+            ConcRV _eps _mm (fromIntegral -> rv) -> do
+              unless (dbug args == 0) $
+                dbugM $ "Obtained concrete return value from main(): " ++ show rv
+              _ <- exitWith (if rv == 0 then ExitSuccess else ExitFailure rv)
+              return ()
+  case memType of
+    BitBlastDagBased -> do
+      (sbe, mem) <- createDagAll be lc mg --first (sbeBitBlast lc be) <$> createDagMemModel lc be mg
+      processRslt sbe =<< lssImpl sbe mem cb argv' memType args
+    BitBlastBuddyAlloc -> do
+       (sbe, mem) <- createBuddyAll be lc mg
+       processRslt sbe =<< lssImpl sbe mem cb argv' memType args
+
