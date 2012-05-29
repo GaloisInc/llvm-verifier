5559ed4b0a760769219026e66b8ace08843b397d
Aaron Tomb <atomb@galois.com>
2012-05-29 16:11:07 -0700

    Use LSS wrapper for problematic test.

--- test/src/Tests/Errors.hs
+++ test/src/Tests/Errors.hs
@@ -16,28 +16,28 @@ import           Tests.Common
 errorTests :: [(Args, Property)]
 errorTests =
   [
-    lssTest 0 "test-error-paths-all" $ \v cb -> do
+    lssTest 0 "ctests/test-error-paths-all" $ \v cb -> do
       runTestLSSBuddy v cb [] $ chkLSS (Just 2) Nothing
       runTestLSSDag v cb []   $ chkLSS (Just 2) Nothing
 
-  , lssTest 0 "test-error-paths-some" $ \v cb -> do
+  , lssTest 0 "ctests/test-error-paths-some" $ \v cb -> do
       runTestLSSBuddy v cb [] $ chkLSS (Just 1) (Just 0)
       runTestLSSDag v cb [] $ chkLSS (Just 1) (Just 0)
 
-  , lssTest 0 "test-error-paths-some-more" $ \v cb -> do
+  , lssTest 0 "ctests/test-error-paths-some-more" $ \v cb -> do
       runTestLSSBuddy v cb [] $ chkLSS (Just 2) (Just 0)
       runTestLSSDag v cb []   $ chkLSS (Just 2) (Just 0)
 
-  , lssTest 0 "test-error-paths-bad-mem-trivial" $ \v cb -> do
+  , lssTest 0 "ctests/test-error-paths-bad-mem-trivial" $ \v cb -> do
       runTestLSSBuddy v cb [] $ chkLSS (Just 1) Nothing
       runTestLSSDag v cb []   $ chkLSS (Just 1) Nothing
 
-  , lssTest 0 "test-error-paths-bad-mem-symsize" $ \v cb -> do
+  , lssTest 0 "ctests/test-error-paths-bad-mem-symsize" $ \v cb -> do
       runTestLSSBuddy v cb [] $ chkLSS (Just 1) (Just 1)
        -- This seems to hang in GHCI but not from the command line =/
       runTestLSSDag v cb []   $ chkLSS Nothing (Just 1)
 
-  , lssTest 0 "test-error-paths-bad-mem-diverge" $ \v cb -> do
+  , lssTest 0 "ctests/test-error-paths-bad-mem-diverge" $ \v cb -> do
       runTestLSSBuddy v cb [] $ chkLSS (Just 1) (Just 1)
       runTestLSSDag v cb []   $ chkLSS (Just 1) (Just 1)
   ]
