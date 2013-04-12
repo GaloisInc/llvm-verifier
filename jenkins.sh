2661e7464e1c1b3374b1199f0c408f88de8db995
Aaron Tomb <atomb@galois.com>
2013-04-12 13:37:12 -0700

    More updates to Jenkins scripts.

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,11 @@
 # Jenkins should have "cd LLVM && bash jenkins.sh"
 PATH="$HOME/.cabal/bin:$(eval 'echo $'"$HASKELL_RUNTIME"):$TOOLS:$JDK16:$PATH"
 
-cabal update
-cabal install cabal-dev
-cabal install alex
-cabal install happy
-cabal install c2hs
+#cabal update
+#cabal install cabal-dev
+#cabal install alex
+#cabal install happy
+#cabal install c2hs
 
 cabal --version
 cabal-dev --version
@@ -20,6 +20,6 @@ git clone ssh://builder@src.galois.com/llvm-pretty ./build/llvm-pretty
 # git clone ssh://builder@src.galois.com/Aiger ./build/Aiger
 # cabal-dev add-source ./build/abcBridge
 # cabal-dev add-source ./build/Aiger
-cabal-dev install . ../Verinf ./build/abcBridge ./build/llvm-pretty --reinstall --enable-tests
+cabal-dev install . ../Verinf ../SAWCore ./build/abcBridge ./build/llvm-pretty --reinstall --enable-tests
 ./dist/build/test-llvm/test-llvm
 bash stage.sh
