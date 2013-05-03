91acea58d2346d7e2596e29d70b41caacb96e3e0
Aaron Tomb <aarontomb@gmail.com>
2013-05-02 20:00:44 -0700

    Run cabal update before building LSS.

--- jenkins.sh
+++ jenkins.sh
@@ -2,7 +2,7 @@ set -e
 # Jenkins should have "cd LLVM && bash jenkins.sh"
 PATH="$HOME/.cabal/bin:$(eval 'echo $'"$HASKELL_RUNTIME"):$TOOLS:$JDK16:$PATH"
 
-#cabal update
+cabal update
 #cabal install cabal-dev
 #cabal install alex
 #cabal install happy
